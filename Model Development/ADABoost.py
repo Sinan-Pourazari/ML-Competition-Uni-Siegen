6a0d20dfa04875e39084da78c6ecb34e834337bc
Sinan Pourazari <sinan.pourazari@gmx.de>
2023-11-24 21:35:58 +0100

    ADaboost ver3-5

--- Model Development/ADABoost.py
+++ Model Development/ADABoost.py
@@ -7,19 +7,19 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-import Randomforrest
 
-def Run(X,y):
+
+def Run(X,y,test):
     #make custom randomforest from whch the model starts
     Randomforrest = RandomForestClassifier(random_state=211, n_estimators=100, criterion='entropy', min_samples_split=2)
     # split the data
-    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.20, random_state=42)
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.33, random_state=42)
 
-    ABC= AdaBoostClassifier( estimator= Randomforrest ,n_estimators=100, random_state=211, learning_rate=1, algorithm='SAMME.R')
+    ABC= AdaBoostClassifier( estimator= Randomforrest ,n_estimators=100, random_state=test, learning_rate=1, algorithm='SAMME.R')
     ABC.fit(Xtrain,ytrain)
     pred = ABC.predict(Xtest)
 
-    score = f1_score(ytest, pred)
+    score = f1_score(ytest, pred, average='macro')
     return score
 
 
@@ -32,6 +32,8 @@ labels = pd.read_csv('train_label.csv')
 features = features.drop(['Id'], axis=1)
 labels = labels.drop(['Id'], axis=1)
 
+#basic pre prosessing
+#features.drop(inplace=True, labels= ['feature_15'], axis=1)
 
 
 
@@ -44,8 +46,19 @@ labels=labels.to_numpy().flatten()
 selectorVariance= VarianceThreshold()
 features = selectorVariance.fit_transform(features)
 
-#for what ever reason this decraees the performance by arround 2.6%
-selectorVariance= VarianceThreshold()
-features = selectorVariance.fit_transform(features)
+''' 
+#loop to try the imact of the randomstate on the model
+bestrdmscore=0
+bestrdm=0
+for i in range(10000):
+    temp=Run(features, labels, i)
+    print("f1-Score: ", temp, "randomstate: ",i)
+    if temp > bestrdmscore:
+        bestrdmscore=temp
+        bestrdm=i
+print("Best: ", bestrdmscore, " "," state: ",bestrdm)
+#Best:  0.7846610066214404    state:  5591
+'''
+print(Run(features, labels, 710))
 
-print("f1-Score: ", Run(features, labels))
+
--- Model Development/Model versions for Kaggle Submit/ADABoostCKaggle.py
+++ Model Development/Model versions for Kaggle Submit/ADABoostCKaggle.py
@@ -7,44 +7,46 @@ from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-def Run(Xtrain,ytrain, Xtest):
-    # split the data
 
+
+def Run(Xtrain,ytrain,Xtest):
+    #make custom randomforest from whch the model starts
     Randomforrest = RandomForestClassifier(random_state=211, n_estimators=100, criterion='entropy', min_samples_split=2)
 
-    ABC= AdaBoostClassifier(estimator=Randomforrest, n_estimators=100, random_state=211, learning_rate=1, algorithm='SAMME.R')
+    ABC= AdaBoostClassifier( estimator= Randomforrest ,n_estimators=100, random_state=5591, learning_rate=1, algorithm='SAMME.R')
     ABC.fit(Xtrain,ytrain)
     pred = ABC.predict(Xtest)
 
     return pred
-
-
 #read csv
 features = pd.read_csv('train_features.csv')
 labels = pd.read_csv('train_label.csv')
-featuresTest = pd.read_csv('test_features.csv')
+testFeatures =pd.read_csv('test_features.csv')
+
 
 #drop the id
 features = features.drop(['Id'], axis=1)
 labels = labels.drop(['Id'], axis=1)
-featuresTest = featuresTest.drop(['Id'], axis=1)
-featuresTest = featuresTest.drop(['feature_2'], axis=1)
+testFeatures.drop(inplace=True, labels=['Id','feature_2'], axis=1)
+
+#basic pre prosessing
+#features.drop(inplace=True, labels= ['feature_2',], axis=1)
+
+
+
 
 #convert to numpyarray
 features=features.to_numpy()
 labels=labels.to_numpy().flatten()
-featuresTest = featuresTest.to_numpy()
+
 #basic preprossesing
 selectorVariance= VarianceThreshold()
 features = selectorVariance.fit_transform(features)
-
-
 #for what ever reason this decraees the performance by arround 2.6%
 selectorVariance= VarianceThreshold()
 features = selectorVariance.fit_transform(features)
 
-result = Run(features, labels,featuresTest)
-
+result = Run(features,labels,testFeatures)
 #export as csv file
 #loop that makes the id for the predicited valus
 idarr = np.array([])
@@ -55,4 +57,4 @@ return_value=pd.DataFrame({'Id': idarr, 'label': result})
 return_value=return_value.astype(int)
 print(return_value)
 #save it as file
-return_value.to_csv('ABC2.csv', columns=['Id', 'label'], index=False)
+return_value.to_csv('ABC5.csv', columns=['Id', 'label'], index=False)
