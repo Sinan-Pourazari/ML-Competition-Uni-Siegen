ac03178931ecc8c3a4c08dd69caa5f667b95c57f
Sinan Pourazari <sinan.pourazari@gmx.de>
2023-11-25 14:22:29 +0100

    ADaboost ver 7 made withndifferent hyperparameters near standart tryed with grid search. no imporvment

--- Model Development/Adaboost Gridsearch test.py
+++ Model Development/Adaboost Gridsearch test.py
@@ -1,11 +1,13 @@
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score,make_scorer
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import scale
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+
 
 
 
@@ -16,6 +18,28 @@ def Run(X,y,test):
     Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.24, random_state=211)
 
     ABC= AdaBoostClassifier( estimator= Randomforrest ,n_estimators=50, random_state=5591, learning_rate=1, algorithm='SAMME.R')
+    # Define the parameter grid for the grid search
+    n_estimators_array = [];
+    min_samples_split_array = [];
+    for i in range(2, 200, 5):
+        n_estimators_array.append(i)
+
+    for i in range(2, 20):
+        min_samples_split_array.append(i)
+    param_grid = {
+        'n_estimators': n_estimators_array,
+        'learning_rate': [0.9, 1.0],
+        #'criterion': ['entropy', 'log_loss', 'gini'],
+        #'min_samples_split': min_samples_split_array,
+        'algorithm': ['SAMME.R', 'SAMME']
+
+    }
+    f1_scorer = make_scorer(f1_score, average='macro')
+    grid_search = GridSearchCV(estimator=ABC, param_grid=param_grid, cv=5, scoring=f1_scorer)
+    grid_search.fit(features, labels)
+    best_params = grid_search.best_params_
+    print("Best Parameters:", best_params)
+
     ABC.fit(Xtrain,ytrain)
     pred = ABC.predict(Xtest)
 
@@ -36,7 +60,6 @@ labels = labels.drop(['Id'], axis=1)
 #features.drop(inplace=True, labels= ['feature_13'], axis=1)
 
 
-
 #convert to numpyarray
 features=features.to_numpy()
 labels=labels.to_numpy().flatten()
@@ -44,6 +67,7 @@ labels=labels.to_numpy().flatten()
 #basic preprossesing
 selectorVariance= VarianceThreshold()
 features = selectorVariance.fit_transform(features)
+
 
 ''' 
 #loop to try the imact of the randomstate on the model
--- Model Development/Model versions for Kaggle Submit/ADABoostCKaggle.py
+++ Model Development/Model versions for Kaggle Submit/ADABoostCKaggle.py
@@ -13,7 +13,7 @@ def Run(Xtrain,ytrain,Xtest):
     #make custom randomforest from whch the model starts
     Randomforrest = RandomForestClassifier(random_state=211, n_estimators=50, criterion='entropy', min_samples_split=10)
 
-    ABC= AdaBoostClassifier( estimator= Randomforrest ,n_estimators=50, random_state=5591, learning_rate=1, algorithm='SAMME.R')
+    ABC= AdaBoostClassifier( estimator= Randomforrest ,n_estimators=7, random_state=5591, learning_rate=0.9, algorithm='SAMME.R')
     ABC.fit(Xtrain,ytrain)
     pred = ABC.predict(Xtest)
 
@@ -55,4 +55,4 @@ return_value=pd.DataFrame({'Id': idarr, 'label': result})
 return_value=return_value.astype(int)
 print(return_value)
 #save it as file
-return_value.to_csv('ABC6.csv', columns=['Id', 'label'], index=False)
+return_value.to_csv('ABC7.csv', columns=['Id', 'label'], index=False)
