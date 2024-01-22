5797bb00ba744ee9b7856ca9c6c53acdaa19fba5
Sinan Pourazari <sinan.pourazari@gmx.de>
2024-01-22 15:36:41 +0100

    used for gbc53

--- Model_Development/GradientBoostingIMBLearnSmac.py
+++ Model_Development/GradientBoostingIMBLearnSmac.py
@@ -7,21 +7,8 @@ from ConfigSpace import ConfigurationSpace, Configuration
 from smac import HyperparameterOptimizationFacade, Scenario
 from sklearn.metrics import accuracy_score
 import numpy as np
-def Run(Xtrain,ytrain):
+from Custom_Methods import*
 
-    #define model parameters
-    GBmodel = GradientBoostingClassifier(n_estimators=100,learning_rate=0.1,max_depth=10, random_state=211,verbose=True,
-                                        loss='log_loss', criterion='friedman_mse')
-
-    # split the data
-    GBmodel.fit(Xtrain, ytrain)
-
-
-    #make Prediction
-    #1 dump(GBmodel, 'Model versions for Kaggle Submit/GBmodel6_wo_f2_downsamp_IMBLearn_NearMiss')
-    scores = cross_val_score(GBmodel, Xtrain, ytrain, cv=5, scoring='f1_macro', n_jobs=-1)
-    #print(scores)
-    print('scores: ', scores,"%0.2f F1-Macro with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
 
 
 #read csv
@@ -29,25 +16,23 @@ features = pd.read_csv('train_features.csv')
 labels = pd.read_csv('train_label.csv')
 
 
-#drop the id
-features = features.drop(['Id'], axis=1)
-labels = labels.drop(['Id'], axis=1)
+features = features[['feature_24', 'feature_16', 'feature_19', 'feature_11', 'feature_26', 'feature_9', 'feature_0', 'feature_10', 'feature_30', 'feature_25']]
+labels = labels[['feature_24', 'feature_16', 'feature_19', 'feature_11', 'feature_26', 'feature_9', 'feature_0', 'feature_10', 'feature_30', 'feature_25']]
+
+features, labels = removeOutlier(features,labels)
 
 
-#drop features without information
-
-#features.drop(['feature_2'], axis=1, inplace=True)
 
 #resample using imbalanced learn
 labels=labels.to_numpy().flatten()
 
 #up and down sampling
 
-'''smo = SMOTE(random_state=42)
+smo = SMOTE(random_state=42)
 X_res, y_res = smo.fit_resample(features, labels)
 features =X_res
 labels = y_res
-labels = pd.DataFrame(labels)'''
+labels = pd.DataFrame(labels)
 
 #convert to numpyarray
 #features=features.to_numpy()
